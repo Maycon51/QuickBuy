b0ff8b3a5e031eee7ad6e4fa2deb2d224f0e8813
maycon douglas <may_con51@hotmail.com>
2020-07-14 22:12:46 -0300

    Alterado o quickbuy repositorio, criado o pontape, string de configuração

--- QuickBuy.Web/Startup.cs
+++ QuickBuy.Web/Startup.cs
@@ -3,24 +3,38 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using QuickBuy.Repositorio.Contexto;
 
 namespace QuickBuy.Web
 {
-    public class Startup
+    public class Startup//ponta pé para iniciar todo aplicativo, ele que traz as biblio(repositorio e dominio)
     {
+        public IConfiguration Configuration { get; }
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonFile("config.json",optional:false, reloadOnChange:true);//determina a configuração pro caminho do banco
+            //ele não é opcional é obrigatorio //se alterar no config.json ele vai recarregar automatico
+           
+            Configuration = builder.Build();
+            //ele vai construir uma interface de configuração com base no add.json
+            //o que ele vai passar é config.json pro configuration na hora de iniciar
         }
 
-        public IConfiguration Configuration { get; }
+        
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            var connectionString = Configuration.GetConnectionString("QuickBuyDB");
+            //ele esta percorrendo a string de conexão procurando o quickbuydb
+            services.AddDbContext<QuickBuyContexto>(option => option.UseMySql(connectionString,m => m.MigrationsAssembly("QuickBuy.Repositorio")));
+            //vai ser responsavel por abrir a conexão com o banco de dados
+
 
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
