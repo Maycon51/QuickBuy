94ac69edb0966b7793431cc88e4bfd92c4a25153
Maycon51 <57047513+Maycon51@users.noreply.github.com>
2020-08-02 13:08:43 -0300

    Tela de login ao usuário

--- QuickBuy.Web/ClientApp/src/app/app.module.ts
+++ QuickBuy.Web/ClientApp/src/app/app.module.ts
@@ -9,7 +9,8 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
-import { ProdutoComponent } from './produto/produto.component'
+import { ProdutoComponent } from './produto/produto.component';
+import { LoginComponent } from './usuario/login/login.component'
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { ProdutoComponent } from './produto/produto.component'
     HomeComponent,
     CounterComponent,
     FetchDataComponent,
-    ProdutoComponent //tenho que declarar em tudo pra evitar problema pq é decorator configurando os comp
+    ProdutoComponent, //tenho que declarar em tudo pra evitar problema pq é decorator configurando os comp
+    LoginComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -28,7 +30,8 @@ import { ProdutoComponent } from './produto/produto.component'
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
-      { path: 'produto', component: ProdutoComponent }//definindo o caminho
+      { path: 'produto', component: ProdutoComponent },//definindo o caminho
+      { path: 'entrar', component: LoginComponent }
     ])
   ],
   providers: [],
--- QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,1 +1,15 @@
-﻿
+import { Component } from "@angular/core"//importando do nucleo do angular
+//usamos esse import para que nossa classe trabalhe como componente no angular
+//tem que declarar esse component  na app module que é o principal arquivo do TS pra funcionar
+
+@Component//decorator componente que vai injetar o produtocomponent como componente
+  ({
+    selector: "app-login",//nome da tag que será reenderizado
+    templateUrl: "./login.component.html",
+    styleUrls: ["./login.component.css"]//dentro de chaves pra definir que é uma lista e posso add mais
+  })//precisamos definir qual caminho vai ser encontrado o componente
+
+export class LoginComponent
+{
+
+}
